0293535c2e2857eb2a17390fe38f7f731deea6ba
Guillermo Sesé Santos <guillermosesesantos@hotmail.com>
2020-05-23 15:00:07 +0200

    paciente terminado

--- backend/src/com/jaime/dao/PacienteDaoImpl.java
+++ backend/src/com/jaime/dao/PacienteDaoImpl.java
@@ -81,16 +81,77 @@ public class PacienteDaoImpl implements Dao<Paciente, String> {
 
     @Override
     public Paciente findById(String id) {
-        return null;
+
+        String sql = "SELECT * FROM `paciente` WHERE `num_seg_soc` = ?";
+        ArrayList<Paciente> lstPacientes = null;
+
+        try {
+
+            pst = this.motor.connect().prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            lstPacientes = new ArrayList<>();
+
+            while (rs.next()) {
+                Paciente p = new Paciente();
+                p.setNumSegSoc(rs.getString(1));
+                p.setNombre(rs.getString(2));
+                p.setTotalVisitas(rs.getInt(3));
+
+                lstPacientes.add(p);
+            }
+
+        } catch (SQLException e) {
+
+        } finally {
+            motor.disconnect();
+        }
+
+        Paciente p = null;
+
+        if (lstPacientes != null && !lstPacientes.isEmpty()) {
+            p = lstPacientes.get(0);
+        }
+        return p;
     }
 
     @Override
     public int update(Paciente bean) {
-        return 0;
+        String sql = "UPDATE `paciente` SET `nombre` = ?, `total_visitas` =  ? WHERE `num_seg_soc` = ?";
+        int response = 0;
+
+        try {
+            pst = this.motor.connect().prepareStatement(sql);
+            pst.setString(1, bean.getNombre());
+            pst.setInt(2, bean.getTotalVisitas());
+            pst.setString(3, bean.getNumSegSoc());
+
+            response = this.motor.execute(pst);
+
+        } catch (SQLException e) {
+
+        } finally {
+            this.motor.disconnect();
+        }
+        return response;
     }
 
     @Override
     public int delete(String id) {
-        return 0;
+        String sql = "DELETE FROM `paciente` WHERE `num_seg_soc` = ?";
+        int response = 0;
+
+        try {
+            pst = this.motor.connect().prepareStatement(sql);
+            pst.setString(1, id);
+
+            response = this.motor.execute(pst);
+
+        } catch (SQLException e) {
+
+        } finally {
+            this.motor.disconnect();
+        }
+
+        return response;
     }
 }
--- backend/src/com/jaime/models/Paciente.java
+++ backend/src/com/jaime/models/Paciente.java
@@ -49,6 +49,11 @@ public class Paciente {
         this.totalVisitas = totalVisitas;
     }
 
+    public Paciente totalVisitas(Integer totalVisitas) {
+        this.totalVisitas = totalVisitas;
+        return this;
+    }
+
     @Override
     public String toString() {
         return "Paciente{" +
